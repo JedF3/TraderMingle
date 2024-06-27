73038aabd66be22cd7984d264fb74b893b5e7843
jedidiahf740 <jedidiahf740@gmail.com>
2024-06-27 18:05:17 +0800

    fix: Added asyncHandler

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -1,17 +1,18 @@
 import listings from "../models/listing.model.js";
+import { asyncHandler } from "../middleware/errorHandler.js";
 
-const getListings = async(req, res, next)=>{
+const getListings = asyncHandler(async(req, res, next)=>{
     const allListings = await listings.find();
     res.status(200).send({message:"Retrieved all listings", data:allListings});
-};
+});
 
-const searchForListing = async(req, res, next)=>{
+const searchForListing = asyncHandler(async(req, res, next)=>{
     const searchTerm = new RegExp(req.params.searchTerm);
     const searchedListings = await listings.find({$or:[{title:{$regex:searchTerm}}, {description:{$regex:searchTerm}}]});
     res.status(200).send({message:"Retrieved all listings", data:searchedListings});
-};
+});
 
-const addListing = async(req, res, next)=>{
+const addListing = asyncHandler(async(req, res, next)=>{
     const {userID, title, description, price, category, isSold, createDate} = req.body;
     let newListing;
     if(req.files){
@@ -42,13 +43,14 @@ const addListing = async(req, res, next)=>{
     }
     await newListing.save();
     res.status(201).send({message:"New listing posted!", data:newListing});
-}
-const viewOneListing = async(req, res, next)=>{
+})
+
+const viewOneListing = asyncHandler(async(req, res, next)=>{
     const selectedListing = await listings.findOne({_id:req.params.id}).populate({path:"userID", select:"username"});
     res.status(200).send({message:"Retrieved Listing", data:selectedListing});
-}
+})
 
-const editListing = async(req, res, next)=>{
+const editListing = asyncHandler(async(req, res, next)=>{
     const listingID = req.params.id;
     const target = await listings.findOne({_id:listingID});
     if(target){
@@ -74,8 +76,8 @@ const editListing = async(req, res, next)=>{
     else{
         res.status(404).send({Message:"Target not found"});
     }
-}
-const deleteListing = async(req, res, next)=>{
+})
+const deleteListing = asyncHandler(async(req, res, next)=>{
     const listingID = req.params.id;
     const target = await listings.findOne({_id:listingID});
     if(target){
@@ -85,5 +87,5 @@ const deleteListing = async(req, res, next)=>{
     else{
         res.status(404).send({message:"Listing not found"});
     }
-}
+})
 export {getListings, addListing, viewOneListing, editListing, searchForListing, deleteListing};
