e9eb5a2800da59859476614ea57e7d8458d5396d
jedidiahf740 <jedidiahf740@gmail.com>
2024-06-27 17:56:03 +0800

    fix: Added required auth

--- backend/routes/listings.js
+++ backend/routes/listings.js
@@ -2,14 +2,15 @@ import { Router } from "express";
 import { addListing, deleteListing, editListing, getListings, searchForListing, viewOneListing } from "../controllers/listing.controller.js";
 import multer from "multer";
 import {storage} from "../config/listingStorage.js";
+import requireAuth from "../middleware/requireAuth.js";
 
 const baseURL="/api/v1/users";
 const listingsRouter = Router();
 const listingImages = multer({storage})
-listingsRouter.post("/addListing", listingImages.array("listing-img"), addListing);
+listingsRouter.post("/addListing", listingImages.array("listing-img"), requireAuth, addListing);
 listingsRouter.get("/viewAllListings", getListings);
 listingsRouter.get("/viewListing/:id", viewOneListing);
 listingsRouter.get("/search/:searchTerm", searchForListing);
-listingsRouter.put("/editListing/:id", listingImages.array("listing-img"), editListing);
-listingsRouter.put("/deleteListing/:id", deleteListing);
+listingsRouter.put("/editListing/:id", listingImages.array("listing-img"), requireAuth, editListing);
+listingsRouter.put("/deleteListing/:id", requireAuth, deleteListing);
 export default listingsRouter;
