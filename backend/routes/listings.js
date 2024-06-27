0d0016d83c74138e802236de31d1902dc33f7bb9
jedidiahf740 <jedidiahf740@gmail.com>
2024-06-27 17:39:11 +0800

    fix: modified endpoint url

--- backend/routes/listings.js
+++ backend/routes/listings.js
@@ -7,7 +7,7 @@ const baseURL="/api/v1/users";
 const listingsRouter = Router();
 const listingImages = multer({storage})
 listingsRouter.post("/addListing", listingImages.array("listing-img"), addListing);
-listingsRouter.get("/viewListing", getListings);
+listingsRouter.get("/viewAllListings", getListings);
 listingsRouter.get("/viewListing/:id", viewOneListing);
 listingsRouter.get("/search/:searchTerm", searchForListing);
 listingsRouter.put("/editListing/:id", listingImages.array("listing-img"), editListing);
