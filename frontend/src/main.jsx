e2439a167021ae2f153b5d0de81e265fc6800983
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-07-04 14:43:13 +0800

    fix: removed stict mode from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,9 @@ import { AuthContextProvider } from './context/AuthContext';
 import { UserProfileContextProvider } from './context/UserProfileContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <UserProfileContextProvider>
-        <App />
-      </UserProfileContextProvider>
-    </AuthContextProvider>
-  </React.StrictMode>
+  <AuthContextProvider>
+    <UserProfileContextProvider>
+      <App />
+    </UserProfileContextProvider>
+  </AuthContextProvider>
 );
